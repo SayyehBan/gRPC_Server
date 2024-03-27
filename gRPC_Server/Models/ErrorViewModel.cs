805e10eeb4e2bcab7db74c5049db7f6a5a149c56
SayyehBan <sdvp1991david@gmail.com>
2024-03-27 15:38:11 +0330

    Add project files.

---
+++ gRPC_Server/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace gRPC_Server.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
